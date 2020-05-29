85033747027ce0dd9b5c8123e416e573e9cb55f8
matipolom <66021890+matipolom@users.noreply.github.com>
2020-05-29 17:08:15 +0200

    Add files via upload
    ZAKTUALIZOWANE 29.05 
    ZROBIŁEM PRZYCISKI I WPISYWANEI TEKSTU PLUS RYSUNKI, BRAK ZDARZEŃ I TEXTOUT DLA WARTOSCI MASY

--- WindowsProject1/WindowsProject1/WindowsProject1.cpp
+++ WindowsProject1/WindowsProject1/WindowsProject1.cpp
@@ -4,22 +4,36 @@
 using namespace Gdiplus;
 #pragma comment (lib,"Gdiplus.lib")
 
+
+
 VOID OnPaint(HDC hdc)
 {
 	Graphics graphics(hdc);
-	Pen      pen(Color(255, 0, 0, 255));
+	Pen      pen(Color(255, 0, 0, 0), 10);
+	graphics.DrawLine(&pen, 0, 180, 200, 180);
+	graphics.DrawLine(&pen, 0, 330, 200, 330);
+	graphics.DrawLine(&pen, 0, 480, 200, 480);
+	graphics.DrawLine(&pen, 0, 630, 200, 630 );
+	graphics.DrawRectangle(&pen, 210, 10, 170, 700);
+	graphics.DrawRectangle(&pen, 230, 30, 130, 150);
 	
+
 }
+
+
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 {
+	
 	HWND                hWnd;
 	MSG                 msg;
 	WNDCLASS            wndClass;
 	GdiplusStartupInput gdiplusStartupInput;
 	ULONG_PTR           gdiplusToken;
+
+
 
 	// Initialize GDI+.
 	GdiplusStartup(&gdiplusToken, &gdiplusStartupInput, NULL);
@@ -39,7 +53,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 
 	hWnd = CreateWindow(
 		TEXT("GettingStarted"),   // window class name
-		TEXT("Getting Started"),  // window caption
+		TEXT("WINDA"),  // window caption
 		WS_OVERLAPPEDWINDOW,      // window style
 		CW_USEDEFAULT,            // initial x position
 		CW_USEDEFAULT,            // initial y position
@@ -49,6 +63,78 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, PSTR, INT iCmdShow)
 		NULL,                     // window menu handle
 		hInstance,                // program instance handle
 		NULL);                    // creation parameters
+
+	HWND hwndButton1 = CreateWindow(
+		L"BUTTON",  // Predefined class; Unicode assumed 
+		L"1",      // Button text 
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+		10,         // x position 
+		550,         // y position 
+		50,        // Button width
+		50,        // Button height
+		hWnd,     // Parent window
+		NULL,       // No menu.
+		(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+		NULL);      // Pointer not needed.
+
+	HWND hwndTextButton1 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT("0"),
+		WS_CHILD | WS_VISIBLE, 100, 550, 40,
+		20, hWnd, NULL, NULL, NULL);
+
+
+	HWND hwndButton2 = CreateWindow(
+		L"BUTTON",  // Predefined class; Unicode assumed 
+		L"2",      // Button text 
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+		10,         // x position 
+		400,         // y position 
+		50,        // Button width
+		50,        // Button height
+		hWnd,     // Parent window
+		NULL,       // No menu.
+		(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+		NULL);      // Pointer not needed.
+
+
+	HWND hwndTextButton2 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT("0"),
+		WS_CHILD | WS_VISIBLE, 100, 400, 40,
+		20, hWnd, NULL, NULL, NULL);
+
+	HWND hwndButton3 = CreateWindow(
+		L"BUTTON",  // Predefined class; Unicode assumed 
+		L"3",      // Button text 
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+		10,         // x position 
+		250,         // y position 
+		50,        // Button width
+		50,        // Button height
+		hWnd,     // Parent window
+		NULL,       // No menu.
+		(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+		NULL);      // Pointer not needed.
+
+
+	HWND hwndTextButton3 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT("0"),
+		WS_CHILD | WS_VISIBLE, 100, 250, 40,
+		20, hWnd, NULL, NULL, NULL);
+
+	HWND hwndButton4 = CreateWindow(
+		L"BUTTON",  // Predefined class; Unicode assumed 
+		L"4",      // Button text 
+		WS_TABSTOP | WS_VISIBLE | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+		10,         // x position 
+		100,         // y position 
+		50,        // Button width
+		50,        // Button height
+		hWnd,     // Parent window
+		NULL,       // No menu.
+		(HINSTANCE)GetWindowLongPtr(hWnd, GWLP_HINSTANCE),
+		NULL);      // Pointer not needed.
+
+
+	HWND hwndTextButton4 = CreateWindowEx(WS_EX_CLIENTEDGE, TEXT("Edit"), TEXT("0"),
+		WS_CHILD | WS_VISIBLE, 100, 100, 40,
+		20, hWnd, NULL, NULL, NULL);
 
 	ShowWindow(hWnd, iCmdShow);
 	UpdateWindow(hWnd);
